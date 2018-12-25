ee11f0a51879ab2de3f25724255b969d39a06f63
pavankumar.ror <pavan.kumar@cortexcraft.com>
2018-12-25 19:36:05 +0530

    First commit

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,10 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "8bfeb83b61a8d883c2d931600b438cc4"
+  config.secret = "e4bc5ae2647d81df89e7c85fcc443f98"
+  config.scope = "read_products" # Consult this page for more scope options:
+                                 # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
